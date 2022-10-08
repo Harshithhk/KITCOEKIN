f34a0d3e358fa7831988076cac4daa819f738cda
swapnil jadhav <swapnilhgf@gmail.com>
2022-10-08 14:52:35 +0530

    minor

--- pages/index.js
+++ pages/index.js
@@ -56,14 +56,14 @@ const Home = (props) => {
       <NewsAndNotices news={props.news} notices={props.notices} />
       <Overviews />
       <div className="h-36 mt-20 flex gap-[150px] justify-center items-center">
-        <a href="#_" className="relative px-10  py-5 font-medium text-white group">
+        <a href="apply-admission" className="relative px-10  py-5 font-medium text-white group">
           <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-violet-500 group-hover:bg-violet-700 group-hover:skew-x-12"></span>
           <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-violet-700 group-hover:bg-violet-500 group-hover:-skew-x-12"></span>
           <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-violet-600 -rotate-12"></span>
           <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-violet-400 -rotate-12"></span>
           <span className="relative">Admission Enquery</span>
         </a>
-        <a href="#_" className="relative px-10  py-5 font-medium text-white group">
+        <a href="apply-admission" className="relative px-10  py-5 font-medium text-white group">
           <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-violet-500 group-hover:bg-violet-700 group-hover:skew-x-12"></span>
           <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-violet-700 group-hover:bg-violet-500 group-hover:-skew-x-12"></span>
           <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-violet-600 -rotate-12"></span>
