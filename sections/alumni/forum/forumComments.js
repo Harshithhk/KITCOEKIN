bdf7918dfa738a9200c2f470b770c7495e7c7041
swapnil <swapnilhgf@gmail.com>
2023-04-04 09:10:04 +0530

    input forum color mobile

--- sections/alumni/forum/forumComments.js
+++ sections/alumni/forum/forumComments.js
@@ -10,17 +10,17 @@ const ForumComments = () =>{
       <div className="grid grid-cols-2 ">
       <div className="m-2">
             <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+            <input type="text" id="first_name" class=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
         </div>
         <div className="m-2">
         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
-        <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
+        <input type="email" id="email" class=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
         </div>
       </div>
-          <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+          <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200  dark:border-gray-700">
               <label for="comment" class="sr-only">Your comment</label>
               <textarea id="comment" rows="6"
-                  class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+                  class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 "
                   placeholder="Write a comment..." required></textarea>
           </div>
           <button type="submit"
@@ -28,7 +28,7 @@ const ForumComments = () =>{
               Post comment
           </button>
       </form>
-      <article class="p-6 mb-6 text-base  rounded-lg dark:bg-gray-900">
+      <article class="p-6 mb-6 text-base  rounded-lg ">
           <footer class="flex justify-between items-center mb-2">
               <div class="flex items-center">
                   <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
@@ -41,7 +41,7 @@ const ForumComments = () =>{
                           title="February 8th, 2022">Feb. 8, 2022</time></p>
               </div>
               <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
-                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400  rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400  rounded-lg  focus:ring-4 focus:outline-none focus:ring-gray-50   dark:focus:ring-gray-600"
                   type="button">
                   <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg">
@@ -53,20 +53,20 @@ const ForumComments = () =>{
               </button>
              
               <div id="dropdownComment1"
-                  class="hidden z-10 w-36  rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                  class="hidden z-10 w-36  rounded divide-y divide-gray-100 shadow  dark:divide-gray-600">
                   <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
                       aria-labelledby="dropdownMenuIconHorizontalButton">
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                              class="block py-2 px-4   dark:hover:text-white">Edit</a>
                       </li>
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                              class="block py-2 px-4   dark:hover:text-white">Remove</a>
                       </li>
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                              class="block py-2 px-4   dark:hover:text-white">Report</a>
                       </li>
                   </ul>
               </div>
@@ -82,7 +82,7 @@ const ForumComments = () =>{
               </button>
           </div>
       </article>
-      <article class="p-6 mb-6 ml-6 lg:ml-12 text-base  rounded-lg dark:bg-gray-900">
+      <article class="p-6 mb-6 ml-6 lg:ml-12 text-base  rounded-lg ">
           <footer class="flex justify-between items-center mb-2">
               <div class="flex items-center">
                   <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 w-6 h-6 rounded-full">
@@ -92,7 +92,7 @@ const ForumComments = () =>{
                           title="February 12th, 2022">Feb. 12, 2022</time></p>
               </div>
               <button id="dropdownComment2Button" data-dropdown-toggle="dropdownComment2"
-                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400  rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400  rounded-lg  focus:ring-4 focus:outline-none focus:ring-gray-50   dark:focus:ring-gray-600"
                   type="button">
                   <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg">
@@ -104,20 +104,20 @@ const ForumComments = () =>{
               </button>
              
               <div id="dropdownComment2"
-                  class="hidden z-10 w-36  rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                  class="hidden z-10 w-36  rounded divide-y divide-gray-100 shadow  dark:divide-gray-600">
                   <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
                       aria-labelledby="dropdownMenuIconHorizontalButton">
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                              class="block py-2 px-4   dark:hover:text-white">Edit</a>
                       </li>
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                              class="block py-2 px-4   dark:hover:text-white">Remove</a>
                       </li>
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                              class="block py-2 px-4   dark:hover:text-white">Report</a>
                       </li>
                   </ul>
               </div>
@@ -131,7 +131,7 @@ const ForumComments = () =>{
               </button>
           </div>
       </article>
-      <article class="p-6 mb-6 text-base  border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+      <article class="p-6 mb-6 text-base  border-t border-gray-200 dark:border-gray-700 ">
           <footer class="flex justify-between items-center mb-2">
               <div class="flex items-center">
                   <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 w-6 h-6 rounded-full">
@@ -141,7 +141,7 @@ const ForumComments = () =>{
                           title="March 12th, 2022">Mar. 12, 2022</time></p>
               </div>
               <button id="dropdownComment3Button" data-dropdown-toggle="dropdownComment3"
-                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400  rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400  rounded-lg  focus:ring-4 focus:outline-none focus:ring-gray-50   dark:focus:ring-gray-600"
                   type="button">
                   <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg">
@@ -153,20 +153,20 @@ const ForumComments = () =>{
               </button>
              
               <div id="dropdownComment3"
-                  class="hidden z-10 w-36  rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                  class="hidden z-10 w-36  rounded divide-y divide-gray-100 shadow  dark:divide-gray-600">
                   <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
                       aria-labelledby="dropdownMenuIconHorizontalButton">
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                              class="block py-2 px-4   dark:hover:text-white">Edit</a>
                       </li>
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                              class="block py-2 px-4   dark:hover:text-white">Remove</a>
                       </li>
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                              class="block py-2 px-4   dark:hover:text-white">Report</a>
                       </li>
                   </ul>
               </div>
@@ -180,7 +180,7 @@ const ForumComments = () =>{
               </button>
           </div>
       </article>
-      <article class="p-6 text-base  border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+      <article class="p-6 text-base  border-t border-gray-200 dark:border-gray-700 ">
           <footer class="flex justify-between items-center mb-2">
               <div class="flex items-center">
                   <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 w-6 h-6 rounded-full">
@@ -190,7 +190,7 @@ const ForumComments = () =>{
                           title="June 23rd, 2022">Jun. 23, 2022</time></p>
               </div>
               <button id="dropdownComment4Button" data-dropdown-toggle="dropdownComment4"
-                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400  rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400  rounded-lg  focus:ring-4 focus:outline-none focus:ring-gray-50   dark:focus:ring-gray-600"
                   type="button">
                   <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg">
@@ -201,20 +201,20 @@ const ForumComments = () =>{
               </button>
              
               <div id="dropdownComment4"
-                  class="hidden z-10 w-36  rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                  class="hidden z-10 w-36  rounded divide-y divide-gray-100 shadow  dark:divide-gray-600">
                   <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
                       aria-labelledby="dropdownMenuIconHorizontalButton">
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                              class="block py-2 px-4   dark:hover:text-white">Edit</a>
                       </li>
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                              class="block py-2 px-4   dark:hover:text-white">Remove</a>
                       </li>
                       <li>
                           <a href="#"
-                              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                              class="block py-2 px-4   dark:hover:text-white">Report</a>
                       </li>
                   </ul>
               </div>
